302d758b28dc08d65556fe09dd33d1e664f24073
Pier St <pier.stoyanov@gmail.com>
2020-06-28 00:17:57 +0300

    more djanog stuff

--- bulls_cows/urls.py
+++ bulls_cows/urls.py
@@ -1,6 +1,8 @@
-from django.urls import path
+from django.contrib import admin
+from django.urls import include, path
+
 from bulls_cows import views
 
 urlpatterns = [
-    path("", views.home, name="home"),
+    path("bulls_cows/<name>", views.hello_there, name="hello_there"),
 ]
--- bulls_cows/views.py
+++ bulls_cows/views.py
@@ -2,7 +2,32 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
+import re
+from datetime import datetime
+
 
 def home(request):
 
     return HttpResponse("Hello, Django!")
+
+def hello_there(request, name):
+    now = datetime.now()
+    formatted_now = now.strftime("%A, %d %B, %Y at %X")
+    match_object = re.match(r'[A-Za-z]', name)
+
+    if match_object:
+        clean_name = match_object.group(0)
+    else:
+        clean_name = "Friend"
+        
+    content = "Hello there, " + clean_name + "! It's " + formatted_now
+    return HttpResponse(content)
+
+    return render(
+        request,
+        'bulls_cows/hello_there.html',
+        {
+            'name': name,
+            'date': datetime.now()
+        }
+    )
