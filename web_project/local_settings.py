332b1771e5a301bee8fb3e6c03fdc57ca68e9205
Pier St <pier.stoyanov@gmail.com>
2020-11-27 09:49:44 +0200

    -

---
+++ web_project/local_settings.py
@@ -0,0 +1,9 @@
+# SECURITY WARNING: keep the secret key used in production secret!
+# Old Key
+# SECRET_KEY = '3&&xq-%+##a85dk*fp%a=v=r2%*2i0tzb!^8zuegr^4p)#z+*t'
+
+
+SECRET_KEY = '@1n&0$x1oz&og*ue0n#^8wc_t3wz5gs-iy#-q(ap+qgd%jg=fc'
+
+# from django.core.management.utils import get_random_secret_key
+# print(get_random_secret_key())
